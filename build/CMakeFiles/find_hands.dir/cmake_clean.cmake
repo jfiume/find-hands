4992d020659638a8c08c2cea8c9be129be06b39f
Joe Fiume <joefiume88@gmail.com>
2024-12-08 16:49:14 +0100

    add build files

---
+++ build/CMakeFiles/find_hands.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find_hands.dir/main.cpp.o"
+  "find_hands"
+  "find_hands.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/find_hands.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
